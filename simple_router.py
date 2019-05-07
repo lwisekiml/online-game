511e691aac6146c4c067bff9be3cf0e93d5c4c39
lwisekiml <lwisekiml@naver.com>
2019-05-07 18:19:06 +0900

    simple_router.py complete

--- simple_router.py
+++ simple_router.py
@@ -174,3 +174,82 @@ start_respons 객체에 Content-type과 Content-Length
 로깅/URL 라우팅/정적 리소스 처리까지 기본적인 웹 서버 & WAS가 하는 일을 처리할 수 있는 훌륭한 서버가 되었다.
 
 '''
+
+###########################################################################################
+'''
+완성 코드
+'''
+
+import os, time, sys
+from io import StringIO
+from importlib import import_module
+
+sys.path.append('./')
+
+base_package = 'cgi-bin'
+encoding = 'utf-8'
+default_page = 'index.html'
+extensions_map = {
+    'html':'text/html',
+    'htm':'text/html',
+    'ico':'image/x-icon',
+    'js':'text/javascript',
+    'css':'text/css',
+    'jpg':'image/jpeg',
+    'png':'image/png',
+    'gif':'image/gif',
+    'mp4':'video/mp4',
+    'avi':'video/avi'
+}
+weekdayname = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+monthname = [None, 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+class Response:
+    def __init__(self, stdout):
+        self.stdout = stdout
+    def write(self, contents):
+        print(contents, file = self.stdout)
+
+def route(environ, start_response):
+    stdout = StringIO()
+    path = environ.get('PATH_INFO')[1:] or default_page
+    extension = path[path.rfind('.') + 1:]
+
+    if extension in extensions_map.keys():
+        return do_static(path, extension, start_response)
+
+    return do_dynamic(path, environ, start_response, stdout)
+
+def do_static(path, extension, start_response):
+    try:
+        f = open(path, 'rb')
+    except OSError:
+        start_response('404 File not found', [])
+        return []
+
+    try:
+        fs = os.fstat(f.fileno())
+        start_response('200 OK', [('Content-Type', get_ctype(extension)), ('Content-Length', str(fs[6])), ('Last-Modified', date_time_string(fs.st_mtime))])
+        return f.readlines()
+    except:
+        f.close()
+        raise
+
+def do_dynamic(path, environ, start_response, stdout):
+    import_module(base_package + '.' + path.replace('/', '.')).process(environ.get('REQUEST_PAYLOAD'), Response(stdout))
+    start_response('200 OK', [('Content-Type', 'text/json; charset' + encoding)])
+
+    return [stdout.getvalue().encode(encoding)]
+
+def get_ctype(extension):
+    return extensions_map.get(extension, '')
+
+def date_time_string(self, timestamp=None):
+    if timestamp is None:
+        timestamp = time.time()
+
+    year, month, day, hh, mm, ss, wd, y, z = time.gmtime(timestamp)
+
+    s = '%s, %02d %03s %4d %02d:%02d:%02d GMT' % (weekdayname[wd], day, monthname[month], year, hh, mm, ss)
+
+    return s
